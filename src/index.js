b0d678ab0a0be3f1098e9ef3f1bd89722194a438
Nguyen Ba Truong <truongnguyen1912@gmail.com>
2018-02-22 23:56:34 +0700

    Add log message

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ import 'babel-polyfill';
 import { projectService } from 'services/project';
 import { notificationService } from 'services/notification';
 
-projectService.getTickStatistic().then(projects => notificationService.notify(projects));
+projectService.getTickStatistic().then(projects => {
+    console.log('There are ' + projects.length + ' projects in total');
+
+    notificationService.notify(projects);
+});
